da637a5793e3aa3f3c67e1929cc44d82e2946c68
Carlos Eduardo Almada Freire <carlosalmadafreire@gmail.com>
2025-08-07 00:20:13 -0300

    meu projeto conta banco 2

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,23 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Por favor, digite o número da conta: ");
+        Scanner scanner = new Scanner(System.in);
+        int numero = scanner.nextInt();
+
+        System.out.println("Por favor, digite o nome do titular da conta: ");
+        String nome = scanner.next();
+
+        System.out.println("Por favor, digite a agencia da conta: ");
+        String agencia = scanner.next();
+
+        double saldo = new Random().nextDouble() * 10000; // saldo aleatório entre 0 e 10.000
+        System.out.printf("Seu saldo: %.2f\n", saldo);
+        scanner.close();
+
+        System.out.println("Conta criada com sucesso!");
+        System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo %.2f já está disponível para saque.", nome, agencia, numero, saldo);
     }
 }
